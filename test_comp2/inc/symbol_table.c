156c94a674e53ddfcb3d89e4105b0c6016e6c224
Lucas Mattioli <lucas.mattioli@live.com>
2015-05-01 22:39:06 -0300

    Linked list at... hmm... 60%. Tests after finishing.

--- test_comp2/inc/symbol_table.c
+++ test_comp2/inc/symbol_table.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "symbol_table.h"
 #include "debugger.h"
@@ -8,58 +9,42 @@
 int main(int argc, char **argv) {
 	puts("Hello, world!");
 
-	node *root_node = build_node('F', "main_node", "main_function", "brasil");
-
-	printf("root_node value_type: %c\n", root_node->value_type);
-	printf("root_node identifier: %s\n", root_node->identifier);
-	printf("root_node scope: %s\n", root_node->scope);
-	printf("root_node value: %s\n", root_node->value);
-
 	return 0;
 }
 
-node *build_node(char _value_type, char *_identifier, char *_scope, char *_value) {
-	debug("FUNCTION build_node: Starting...");
+linked_list *new_symbol_table(unsigned int element_size) {
+	assert(element_size == 0);
 
-	node *new_node = (node *) malloc(sizeof(node));
+	debug("FUNCTION new_symbol_table: Starting...");
 
-	if(new_node != NULL) {
-		new_node->value_type = _value_type;
-		new_node->identifier = _identifier;
-		new_node->scope = _scope;
-		new_node->value = _value;
-		new_node->next = new_node->previous = NULL;
+	linked_list *symbol_table = NULL;
+	symbol_table = (linked_list *) malloc(sizeof(linked_list));
 
-		debug("FUNCTION build_node: Node built...");
+	if(symbol_table != NULL) {
+		symbol_table->element_size = element_size;
+		symbol_table->length = 0;
+		symbol_table->head = symbol_table->tail = NULL;
 	}
 	else {
-		puts("An error occurred while building a node!");
+		puts("There was an error while allocating memory with malloc. Do something. Now.\n");
 		exit(0);
 	}
 
-	debug("FUNCTION build_node: Leaving...");
-
-	return new_node;
+	debug("FUNCTION new_symbol_table: Leaving...");
+	
+	return symbol_table;
 }
 
-node *insert_node(char _value_type, char *_identifier, char *_scope, char *_value) {
-
-}
-
-node *search_identifier(char *_identifier) {
-
-}
-
-void free_symbol_table(node *_root_node) {
+void free_symbol_table(linked_list *symbol_table) {
 	debug("FUNCTION free_symbol_table: Starting...");
 
-	node *helper_node = NULL;
-	
-	while(_root_node != NULL) {
-		helper_node = _root_node;
-		_root_node = _root_node->next;
-
-		free(helper_node);
+	node *current_node = NULL;
+	while(symbol_table->head != NULL) {
+		current_node = symbol_table->head;
+		symbol_table->head = current_node->next;
+		
+		free(current_node->data);
+		free(current_node);
 	}
 
 	debug("FUNCTION free_symbol_table: Leaving...");
@@ -67,5 +52,24 @@ void free_symbol_table(node *_root_node) {
 	return;
 }
 
+void insert_element(linked_list *symbol_table, void *element) {
 
+}
+
+unsigned int symbol_table_size(linked_list *symbol_table) {
+	debug("FUNCTION symbol_table_size: Starting...");
+	
+	int table_size = 0;
+	if(symbol_table != NULL) {
+		table_size = symbol_table->length;
+	}
+	else {
+		puts("A empty symbol table has been passed as argument. There is no size.\n");
+		exit(0);
+	}
+
+	debug("FUNCTION symbol_table_size: Leaving...");
+
+	return table_size;
+}
 
--- test_comp2/inc/symbol_table.h
+++ test_comp2/inc/symbol_table.h
@@ -2,16 +2,16 @@
 #define _SYMBOL_TABLE_H_
 
 // A function's pointer that will be used as a generic free function.
-typedef void (*free_function)(void *);
+// typedef void (*free_function)(void *);
 
 
 // A generic node that can handle any data type due to its void pointer (void *) element (data).
-// By building this guy down here, only one implementation of a linked list will be needed.
-typedef struct generic_node {
+// By building this guy down here, only one implementation of a linked list will be necessary.
+typedef struct node {
 	void *data;
 	struct node *next;
 	struct node *previous;
-} generic_node;
+} node;
 
 
 // A helper structure that will control a symbol table (linked list) as a whole.
@@ -20,16 +20,24 @@ typedef struct generic_node {
 // Third and fourth ones are, respectively, a pointer to the first node and a pointer to the last node of the list.
 // The last one is a pointer to a function that will implement a specific way of freeing the specific type of the list.
 typedef struct {
-	int length;
-	int element_size;
-	generic_node *head;
-	generic_node *tail;
-	free_function free_f;
-} symbol_table;
+	unsigned int length;
+	unsigned int element_size;
+	node *head;
+	node *tail;
+	// free_function free_f;
+} linked_list;
 
-node *build_node(char _value_type, char *_identifier, char *_scope, char *_value);
-node *insert_node(char _value_type, char *_identifier, char *_scope, char *_value);
-node *search_identifier(char *_identifier);
-void free_symbol_table(node *_root_node);
+
+// Allocates memory to a linked list and fill its element_size with the first parameter.
+linked_list *new_symbol_table(unsigned int element_size);
+
+// Frees every node of a symbol table. In other words, free the WHOLE symbol table.
+void free_symbol_table(linked_list *symbol_table);
+
+// Inserts a element in the last position of the symbol table.
+void insert_element(linked_list *symbol_table, void *element);
+
+// Returns the symbol table size.
+unsigned int symbol_table_size(linked_list *symbol_table);
 
 #endif
