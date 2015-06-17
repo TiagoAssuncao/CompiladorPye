8f382d65c3de3cd03d1278508ceeb01f82fb0538
Lucas Mattioli <lucas.mattioli@live.com>
2015-06-17 09:46:47 -0300

    Changed stack node definition.

--- pye/inc/stack.h
+++ pye/inc/stack.h
@@ -4,6 +4,8 @@
 //It's a struct for store every names of the scope in a stack
 typedef struct stack_node {
 	char scope_name[35];
+	unsigned int tabulation_level;
+	unsigned int space_level;
 	struct stack_node *next;
 	struct stack_node *previous;
 } stack_node;
@@ -14,7 +16,7 @@ typedef struct {
 } stack_header;
 
 stack_header *new_stack();
-stack_header *insert_scope_on_stack(stack_header *header, char scope_name[35]);
+stack_header *insert_scope_on_stack(stack_header *header, char scope_name[35], unsigned int tabulation_level, unsigned int space_level);
 stack_header *pop_element(stack_header *header);
 stack_node *get_top(stack_header *header);
 void show_stack(stack_header *header);
