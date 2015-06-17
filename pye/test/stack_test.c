87d00e8bd726afde6c820a13e2d87f077e453765
Lucas Costa <lucascostaa73@gmail.com>
2015-06-17 09:29:40 -0300

    Creating test for insert Element on Stack.

--- pye/test/stack_test.c
+++ pye/test/stack_test.c
@@ -4,6 +4,8 @@
 #include <CUnit/Basic.h>
 #include "stack.h"
 
+stack_header *header = NULL;
+
 int init_suite(void)
 {
    // Run commands to prepare test enviroment
@@ -21,10 +23,50 @@ void test(void)
    CU_ASSERT(1 == 1);
 }
 
+/* Test for insert element on stack */
+int init_suiteInsert(void)
+{
+   header = new_stack();
+   header = insert_scope_on_stack(header,"firstScope");
+   return 0;
+}
+int clean_suiteInsert(void)
+{
+   return 0;
+}
+
+/* Test insert Element*/
+void testInsertElement(void)
+{
+   CU_ASSERT(header->length == 1);
+   stack_node *node = get_top(header);
+
+   CU_ASSERT_PTR_NULL(node->next);
+   CU_ASSERT_PTR_NULL(node->previous);
+   CU_ASSERT_STRING_EQUAL(node->scope_name, "firstScope");
+}
+
+void testInsertSecondElement(void)
+{
+   header = insert_scope_on_stack(header,"secondScope");
+
+   CU_ASSERT(header->length == 2);
+   stack_node *node = get_top(header);
+
+   CU_ASSERT_STRING_EQUAL(node->scope_name, "secondScope");   
+   CU_ASSERT_PTR_NULL(node->next);
+   CU_ASSERT_PTR_NOT_NULL(node->previous);
+   
+   stack_node* previousNode = node->previous;
+   CU_ASSERT_STRING_EQUAL(previousNode->scope_name, "firstScope");
+   
+}
+
 int main()
 {
    
     CU_pSuite pSuite = NULL;
+    CU_pSuite pSuiteInsert = NULL;
 
    /* initialize the CUnit test registry */
    if (CUE_SUCCESS != CU_initialize_registry())
@@ -32,15 +74,17 @@ int main()
 
    /* add a suite to the registry */
    pSuite = CU_add_suite("Suite", init_suite, clean_suite);
+   pSuiteInsert = CU_add_suite("Suite Insert Element on Stack", init_suiteInsert, clean_suiteInsert);
 
-   if (NULL == pSuite)
+   if (NULL == pSuiteInsert)
    {
       CU_cleanup_registry();
       return CU_get_error();
    }
 
    /* Suite Free */
-   if( (NULL == CU_add_test(pSuite, "free linked list()", test) ))
+   if( (NULL == CU_add_test(pSuiteInsert, "Insert Element on stack()", testInsertElement) ) 
+      || (NULL == CU_add_test(pSuiteInsert, "Insert Second Element on stack()", testInsertSecondElement) ))
    {
       CU_cleanup_registry();
       return CU_get_error();
